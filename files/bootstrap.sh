f9e71ba859dff64e4265c002defa1b51968390c5
Aaron John Canlas <aj@XyalPC.ajohnsc.com>
2021-04-17 17:46:18 +0800

    added midtermfolder

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -8,7 +8,7 @@ chmod -R 755 /root/midterm/files/midtermfolder
 chown root:root /root/midterm/files/midterm
 chown -R root:root /root/midterm/files/midtermfolder
 cp -a /root/midterm/files/midterm /usr/bin
-cp -ar /root/midterm/files/midterm /etc/
+cp -ar /root/midterm/files/midtermfolder /etc/
 cp -ar /root/midterm/files/instruction /etc/
 cp -ar /root/midterm/files/checker /etc/
 rm -rf /root/midterm
