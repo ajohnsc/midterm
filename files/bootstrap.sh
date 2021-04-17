ebad73da5834193f729663f38f6effaacfaf3bbb
Aaron John Canlas <aj@XyalPC.ajohnsc.com>
2021-04-17 17:42:37 +0800

    first commit

---
+++ files/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Created by: AJ Canlas
+# April 17,2021
+
+chmod 755 /root/midterm/files/midterm
+chmod -R 755 /root/midterm/files/midtermfolder
+chown root:root /root/midterm/files/midterm
+chown -R root:root /root/midterm/files/midtermfolder
+cp -a /root/midterm/files/midterm /usr/bin
+cp -ar /root/midterm/files/midterm /etc/
+cp -ar /root/midterm/files/instruction /etc/
+cp -ar /root/midterm/files/checker /etc/
+rm -rf /root/midterm
